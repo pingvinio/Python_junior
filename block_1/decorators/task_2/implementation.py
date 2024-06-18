ad91b266569a99c45c90f2e63b12ed1fef98ebe0
niko_v <48191654+pingvinio@users.noreply.github.com>
2024-06-18 21:49:19 +1000

    Update implementation.py

--- block_1/decorators/task_2/implementation.py
+++ block_1/decorators/task_2/implementation.py
@@ -1,7 +1,19 @@
-def check_value():
+from block_1.common import (
+    factorial,
+    MyException,
+)
+
+def check_value(function):
     """
     Обертка, проверяющая валидность переданного значения(неотрицательный int).
     В случае валидного значения - передает дальше в функцию,
     в противном случае - выбрасывает исключение MyException.
     """
-    raise NotImplementedError
+    def wrapper(num):
+        if type(num) == int and num >= 0:
+            result = function(num)
+            return result
+        else:
+            raise MyException(Exception)
+
+    return wrapper
