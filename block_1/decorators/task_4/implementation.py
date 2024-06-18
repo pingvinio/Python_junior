eaa0a0d9f863ac84baa899bea4bfc74466c0470d
niko_v <48191654+pingvinio@users.noreply.github.com>
2024-06-18 21:50:15 +1000

    Update implementation.py

--- block_1/decorators/task_4/implementation.py
+++ block_1/decorators/task_4/implementation.py
@@ -1,4 +1,9 @@
-def decorator_maker():
+import time
+from block_1.common import (
+    MyException,
+)
+
+def decorator_maker(times, delay):
     """
     Обертка, которая повторяет вызов функции times раз с паузой delay секунд
     Args:
@@ -8,5 +13,17 @@ def decorator_maker():
     Returns:
         валидное значение (при вызове bool() -> True)
     """
-    raise NotImplementedError
+    def decorator_maker_function(function):
+        def wrapper():
+            for counter in range(1, times + 1):
+                try:
+                    res = function()
+                    return res
+                except Exception:
+                    if counter < times:
+                        time.sleep(delay)
+                    else:
+                        raise MyException(Exception)
+        return wrapper
+    return decorator_maker_function
 
