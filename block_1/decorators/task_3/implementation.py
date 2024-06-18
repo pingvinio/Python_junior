ab20e18102c503cc4157bb110b3a0294bd17bebf
niko_v <48191654+pingvinio@users.noreply.github.com>
2024-06-18 21:49:48 +1000

    Update implementation.py

--- block_1/decorators/task_3/implementation.py
+++ block_1/decorators/task_3/implementation.py
@@ -1,9 +1,14 @@
-def counter():
+int = 0
+def counter(function):
     """
     Обертка для подсчёта количества вызовов обернутой функции.
 
     Returns:
         int - количество вызовов функции.
     """
-
-    raise NotImplementedError
+    def wrapper():
+        global int
+        result = function()
+        int += 1
+        return int
+    return wrapper
