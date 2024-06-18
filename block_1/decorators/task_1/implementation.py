43d8393f8f43230db7cde57c7a4c4020e5b7c1f0
niko_v <48191654+pingvinio@users.noreply.github.com>
2024-06-18 21:48:37 +1000

    Update implementation.py

--- block_1/decorators/task_1/implementation.py
+++ block_1/decorators/task_1/implementation.py
@@ -1,6 +1,20 @@
-def time_execution():
+from math import factorial
+import time
+def time_execution(function):
     """
     Обертка, печатающая время выполнения функции.
     """
+    def wrapper(fun):
+        time_start = time.time()
+        result = function(fun)
+        time_stop = time.time()
+        print(f"Функция выполнилась за - {time_stop - time_start} секунд")
+        return result
 
-    raise NotImplementedError
+    return wrapper
+
+#Проверка
+@time_execution
+def fact(numb):  
+   return factorial(numb)
+print(fact(33))
